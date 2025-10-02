c3a7a1a3e3fcae759a64e488fc619b71c90b8fad
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2025-10-02 01:11:15 -0500

    SwapNo Three Method

---
+++ SwapNo.java
@@ -0,0 +1,39 @@
+// Method 1 (Using Third Variable)
+// public class SwapNo {
+//     public static void main(String[] args){
+//         int a=10,b=5;
+
+//         int temp = a;
+//         a=b;
+//         b=temp;
+//         System.out.println("After Swap a :" + a + " , " + "Swap b :"+b);
+
+//     }
+// }
+
+// Method 2 (Using Arithmatic operator)
+// public class SwapNo {
+//     public static void main(String[] args){
+//         int a=10,b=5;
+
+//         a=a+b;
+//         b=a-b;
+//         a=a-b;
+//         System.out.println("After Swap a :" + a + " , " + "Swap b :"+b);
+
+//     }
+// }
+
+// Method 3 (Using Binary Method)
+// public class SwapNo {
+//     public static void main(String[] args){
+//         int a=20, b=10;
+//         a=a^b;
+//         b=a^b;
+//         a=a^b;
+//         System.out.println("After Swap a :" + a + " , " + "Swap b :"+b);
+
+//     }
+// }
+
+
