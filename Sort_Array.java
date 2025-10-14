4f9ef8c6c9c3950b633c8c991967d43bff81a05d
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2025-10-13 23:37:26 -0500

    Sort_Array

---
+++ Sort_Array.java
@@ -0,0 +1,24 @@
+// Sort an array without using built-in functions.
+
+public class Sort_Array {
+
+    static void sortArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,3,1,6,4,7,9,8};
+        sortArray(arr);
+    }
+}
